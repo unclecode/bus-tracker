9d4cd445e239adad776ad76cb330ecc88d9b2b4b
unclecode <unclecode@unclecosMacBook.lan>
2015-08-10 13:08:50 +0800

    all

--- bus_tracker_01.py
+++ bus_tracker_01.py
@@ -3,17 +3,26 @@ office_lon = -87.668452
 
 
 def read_bus_data():
+	# Read all busses location data from API and return a XML handler
 	pass
 
 def find_candidate(xml, direction = 'south'):
+	# Extract all busses information (BusId, LAT, Direction) of those busses are heading to
+	# the given direction and also they will cross my location
 	pass
 
-def find_nearest_candidates(candidates, distant = 0.5):
+def find_nearest_candidates(candidates, distance_range = 0.5):
+	# search into the given candidate busses and find those busses are closer then given distance
 	pass
 
-def monitor():
+def monitor(candidates, alarm_distance):
+	# Check is there any bus among give candidates that its distance to my location 
+	# is less than alarm_distance? If yes then return that bus 
+
 	return False
 
+def display_candidates(candidates):
+	#print cadidate BusId, LAT, Direction, Distance to my location
 
 print 'DONE :)'
 
